f830632f5778eecbb2a5a76ed5a4bba3a1aed6e8
XiangL5 <xiangl5@andrew.cmu.edu>
2020-04-15 22:23:51 -0800

    bug fix

--- src/renderer/libs/api.js
+++ src/renderer/libs/api.js
@@ -5,13 +5,11 @@ import db from "./datastore";
 export default {
   // 用来向现有服务器上传图片的API
   async scannerUpload(form_data, school_id, scanner_id) {
-    const custom_api = (
+    const API_ROOT = (
       await db.findOne({
         key: "custom_api",
       })
     ).value;
-
-    const API_ROOT = custom_api ? "http://127.0.0.1:8000" : custom_api;
 
     const res = await post(
       API_ROOT + `/schools/${school_id}/scanners/${scanner_id}/upload`,
@@ -23,13 +21,11 @@ export default {
 
   // 更新扫描仪与学校对应关系
   async updateScannerInfo(school_id, scanner_id) {
-    const custom_api = (
+    const API_ROOT = (
       await db.findOne({
         key: "custom_api",
       })
     ).value;
-
-    const API_ROOT = custom_api ? "http://127.0.0.1:8000" : custom_api;
 
     const res = await post(
       API_ROOT + `/schools/${school_id}/scanners/${scanner_id}/update`
@@ -39,13 +35,11 @@ export default {
 
   // 查看App更新情况
   async checkApp() {
-    const custom_api = (
+    const API_ROOT = (
       await db.findOne({
         key: "custom_api",
       })
     ).value;
-
-    const API_ROOT = custom_api ? "http://127.0.0.1:8000" : custom_api;
 
     const res = await get(`${API_ROOT}/check_app`);
     return res;
